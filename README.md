2cf886e3fff430c59570919dd0684589b5da9258
François Wouts <f@zenc.io>
2024-02-07 21:57:17 +1100

    Implement identical logic using Vite
    This removes the VFS fonts because they fail to build with Vite (and likely also with Rollup, which Vite uses under the hood).
    
    See https://github.com/bpampuch/pdfmake/issues/2654 for some potential ideas to resolve this.

--- README.md
+++ README.md
@@ -3,15 +3,12 @@ example of how to bundle `DataTable` and one of its
 extensions (buttons) and to import it dynamically
 in the body of an HTML document.
 
-The final document is `html/index.html`.
+The final document is `package/dist/index.html`.
 
 It is generated with the following commands:
-```
-# create dt_bundle.js
-cd package
-rollup --config 
 
-# inject dt_bundle.js into index.html
-cd ../html
-python replace_dt_src_with_base64_encoding.py
+```
+cd package
+npm install
+npm run build
 ```
--- package/src/dt_with_buttons.js
+++ package/src/dt_with_buttons.js
@@ -1,14 +1,11 @@
-import jQuery from 'jquery';
-import JSZip from 'jszip';
-import pdfMake from 'pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-import DataTable from 'datatables.net-bs5';
-import 'datatables.net-buttons-bs5';
-import 'datatables.net-buttons/js/buttons.html5';
- 
+import DataTable from "datatables.net-bs5";
+import "datatables.net-buttons-bs5";
+import "datatables.net-buttons/js/buttons.html5";
+import jQuery from "jquery";
+import JSZip from "jszip";
+import pdfMake from "pdfmake";
+
 DataTable.Buttons.jszip(JSZip);
 DataTable.Buttons.pdfMake(pdfMake);
- 
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-export { jQuery, DataTable };
+export { DataTable, jQuery };
