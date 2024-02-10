90cc13935dbfcf325ba944fe2f0caca1de418557
Marc Wouts <marc.wouts@gmail.com>
2024-02-10 22:54:11 +0000

    The full example works!

--- package/src/dt_with_buttons.js
+++ package/src/dt_with_buttons.js
@@ -1,15 +1,15 @@
-//import JSZip from 'jszip';
+import JSZip from 'jszip';
 import jQuery from 'jquery';
-//import pdfMake from 'pdfmake';
-// import pdfFonts from 'pdfmake/build/vfs_fonts';
+import pdfMake from 'pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 import DataTable from 'datatables.net-bs5';
-//import 'datatables.net-buttons-bs5';
-//import 'datatables.net-buttons/js/buttons.html5';
+import 'datatables.net-buttons-bs5';
+import 'datatables.net-buttons/js/buttons.html5';
  
-//DataTable.Buttons.jszip(JSZip);
-//DataTable.Buttons.pdfMake(pdfMake);
+DataTable.Buttons.jszip(JSZip);
+DataTable.Buttons.pdfMake(pdfMake);
  
-// pdfMake.vfs = pdfFonts.pdfMake.vfs;
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 // https://stackoverflow.com/questions/71229017/how-do-you-export-globals-in-a-bundle-with-esbuild
 window.$ = jQuery;
