8ce49675481aabc0a77ed30bcdeeaef81956f551
Marc Wouts <marc.wouts@gmail.com>
2024-02-10 22:35:05 +0000

    Try with simpler imports

---
+++ package/src/dt_with_buttons.js
@@ -0,0 +1,14 @@
+//import JSZip from 'jszip';
+import jQuery from 'jquery';
+//import pdfMake from 'pdfmake';
+// import pdfFonts from 'pdfmake/build/vfs_fonts';
+import DataTable from 'datatables.net-bs5';
+//import 'datatables.net-buttons-bs5';
+//import 'datatables.net-buttons/js/buttons.html5';
+ 
+//DataTable.Buttons.jszip(JSZip);
+//DataTable.Buttons.pdfMake(pdfMake);
+ 
+// pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+export { DataTable, jQuery as $};
